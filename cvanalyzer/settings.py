8213177861dfbcc1c24108c4bce5268f99b979d0
Kamil Wysocki <kpl50970@gmail.com>
2026-03-24 14:34:39 +0100

    New port

--- cvanalyzer/settings.py
+++ cvanalyzer/settings.py
@@ -183,7 +183,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ---------------------------------------------------------------------------
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('MAILGUN_SMTP_HOST', 'smtp.mailgun.org')
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get('MAILGUN_SMTP_LOGIN', '')
